c72082ca2b4281979d8bf6586efd980a449acfb0
adarshpatel111 <111566026+adarshpatel111@users.noreply.github.com>
2024-10-16 15:53:50 +0530

    Update Projects.jsx

--- frontEnd/src/Components/Projects/Projects.jsx
+++ frontEnd/src/Components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import { rootColors } from "../../Utilities/rootColors";
 import ProjectCard from "../ProjectCard/ProjectCard";
-import bsrewebsite from "../../assets/bsreWebsiteimg.png";
+import bsrewebsite from "../../assets/BsreWebsiteimg.png";
 import RGAVwebsite from "../../assets/rgavWebsiteimg.png";
 import PhoenixShopping from "../../assets/PhoenixshoppingWebsiteImg.png";
 import ApTechnicalWebsiteImg from "../../assets/ApTechnicalWebsiteImg.png";
